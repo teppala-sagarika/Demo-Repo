673fca90b33d73a1cafb9cc8f0778441c0f25239
Teppala Sagarika <sagarikateppala@gmail.com>
2026-03-07 13:51:46 +0530

    [LeetCode] Minimum Number of Flips to Make the Binary String Alternating · Time: O(n) | Space: O(n) · Lang: cpp · Note: bit manipulation

---
+++ leetcode/Minimum_Number_of_Flips_to_Make_the_Binary_String_Alternating.cpp
@@ -0,0 +1,31 @@
+// ────────────────────────────────────────────────────────────
+// Problem : Minimum Number of Flips to Make the Binary String Alternating
+// Platform: LeetCode
+// URL     : https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/submissions/1940595003/?envType=daily-question&envId=2026-03-07
+// Language: cpp
+// Date    : 3/7/2026, 1:51:18 PM
+// ────────────────────────────────────────────────────────────
+// Time Complexity : O(n)
+// Space Complexity: O(n)
+// Notes           : bit manipulation
+// ────────────────────────────────────────────────────────────
+
+class Solution {
+public:
+    int minFlips(auto& s) {
+        int n = s.length();
+        int res = n;
+        int op[2] = {0, 0};
+
+        for (int i = 0; i < n; i++)
+            op[(s[i] ^ i) & 1]++;
+
+        for (int i = 0; i < n; i++) {
+            op[(s[i] ^ i) & 1]--;
+            op[(s[i] ^ (n + i)) & 1]++;
+            res = min(res, min(op[0], op[1]));
+        }
+
+        return res;
+    }
+};
